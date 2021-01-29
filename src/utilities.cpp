890a5dd6a17f15eab98bfd60a0fa9f021995afee
andrew8077 <andrew.nguyen.18@cnu.edu>
2021-01-29 18:45:28 -0500

    first push

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -16,13 +16,57 @@ using namespace std;
 
 //********************** private to this compilation unit **********************
 
+vector<process_stats> stats;
 //if myString does not contain a string rep of number returns o
 //if int not large enough has undefined behaviour, very fragile
 int stringToInt(const char *myString) {
 	return atoi(myString);
 }
 
+bool checkCorruptedRow(string line){
+	int commas = 0;
+	int values = 0;
+
+	for (unsigned int i = 0; i < line.length(); i++){
+		if (line[i] == CHAR_TO_SEARCH_FOR){
+			commas++;
+		}
+		if (isdigit(line[i])){
+			values++;
+		}
+	}
+	if (commas == 3 && values == 4){
+		return true;
+	}
+	return false;
+}
+
 int loadData(const char* filename, bool ignoreFirstRow) {
+	stats.clear();
+	fstream myInputfile;
+	myInputfile.open(filename);
+
+	if (!myInputfile.is_open()){
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	string line;
+	vector<string> temp;
+	while (!myInputfile.eof()){
+		getline(myInputfile, line);
+		temp.push_back(line);
+	}
+
+	if (ignoreFirstRow){
+		temp.erase(temp.begin());
+	}
+	for (int i = 0; i < temp.size(); i++){
+		if (!checkCorruptedRow){
+
+		}
+	}
+
+	myInputfile.close();
 
 	return SUCCESS;
 }
@@ -41,7 +85,7 @@ process_stats getNext() {
 
 //returns number of process_stats structs in the vector holding them
 int getNumbRows(){
-	return 0;
+	return stats.size();
 }
 
 
