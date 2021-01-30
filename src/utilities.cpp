d36bc27a86a1bfbd39ace182783ccc176188cb71
andrew8077 <andrew.nguyen.18@cnu.edu>
2021-01-30 14:46:04 -0500

    need to fix getNumbRows

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 //********************** private to this compilation unit **********************
 
-vector<process_stats> stats;
+vector<vector<process_stats>> stats;
 //if myString does not contain a string rep of number returns o
 //if int not large enough has undefined behaviour, very fragile
 int stringToInt(const char *myString) {
@@ -41,6 +41,34 @@ bool checkCorruptedRow(string line){
 	return false;
 }
 
+vector<process_stats> getValuesFromString(string line){
+	vector<int> tempVector;
+	vector<process_stats> out;
+
+	string val;
+	int temp = 0;
+	for (unsigned int i = 0; i < line.length(); i++){
+		if (line[i] != CHAR_TO_SEARCH_FOR) {
+			val += line[i];
+		}
+		else {
+			stringstream ss(val);
+			ss >> temp;
+			tempVector.push_back(temp);
+			val = "";
+			temp = 0;
+		}
+	}
+	process_stats myStats = {
+			tempVector[0],
+			tempVector[1],
+			tempVector[2],
+			tempVector[3]
+	};
+	out.push_back(myStats);
+	return out;
+}
+
 int loadData(const char* filename, bool ignoreFirstRow) {
 	stats.clear();
 	fstream myInputfile;
@@ -56,18 +84,24 @@ int loadData(const char* filename, bool ignoreFirstRow) {
 		getline(myInputfile, line);
 		temp.push_back(line);
 	}
+	myInputfile.close();
 
+	// remove first row if ignoreFirstRow is true
 	if (ignoreFirstRow){
 		temp.erase(temp.begin());
 	}
+
+	// search and remove corrupted rows
+	// appends clean rows to process_stats vector
+	vector<vector<int>> val;
 	for (unsigned int i = 0; i < temp.size(); i++){
-		if (!checkCorruptedRow(temp[i])){
-			temp.erase(temp.begin() + (i - 1));
+		if (checkCorruptedRow(temp[i]) == false){
+			temp.erase(temp.begin() + i);
+		}
+		else {
+			stats.push_back(getValuesFromString(temp[i]));
 		}
 	}
-
-	myInputfile.close();
-
 	return SUCCESS;
 }
 
@@ -79,7 +113,8 @@ void sortData(SORT_ORDER mySortOrder) {
 
 process_stats getNext() {
 	process_stats myFirst;
-
+	myFirst = stats[0][0], stats[0][1], stats[0][2], stats[0][3];
+	stats.erase(stats.begin());
 	return myFirst;
 }
 
